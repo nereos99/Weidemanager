c560d65751501e38ad24d71ba523422bbbb60678
nereos99 <soeren.bruchmann@web.de>
2026-05-19 07:48:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    Cache-Version hochzählen bei Code-Änderungen erzwingt Update.
 */
 
-const VERSION = 'v2.0.0';
+const VERSION = 'v2.1.0';
 const SHELL_CACHE = `weidemanager-shell-${VERSION}`;
 const LIBS_CACHE  = `weidemanager-libs-${VERSION}`;
 const TILES_CACHE = `weidemanager-tiles-v1`;
